c908ec79b78f277055dedd30e194060badce4b3e
pezhmansamadi21 <pezhmansamadi21@gmail.com>
2022-08-07 21:01:37 +0430

    initial commit
    My very first IOS App

---
+++ README.md
@@ -0,0 +1,8 @@
+# WarCard App
+
+* [Learning Resource YT](https://youtu.be/09TeUXjzpKs)
+* [My Learning Resource](https://github.com/pejmanS21/swift_progress)
+
+### App UI
+
+![ui](./images/app.gif)
--- WarCard/ViewController.swift
+++ WarCard/ViewController.swift
@@ -2,18 +2,48 @@
 //  ViewController.swift
 //  WarCard
 //
-//  Created by Pejman Samadi on 8/7/22.
+//  Created by Pejman Samadi on 7/18/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+    
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            leftScore = leftScore + 1
+        }
+        else if rightNumber > leftNumber {
+            rightScore = rightScore + 1
+        }
+        
+        leftScoreLabel.text = "\(leftScore)"
+        rightScoreLabel.text = "\(rightScore)"
+    }
+    
 }
 
